160b471fa23c0dc1fe7fe2c998ced39d28731577
Hemanath-V <venkatachalamhemnath@gmail.com>
2025-06-20 19:18:07 +0530

    decorater pattern

--- Week1/DecoratorPatternExample/Main.java
+++ Week1/DecoratorPatternExample/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Notifier baseNotifier = new EmailNotifier();
 
-        // Add SMS and Slack on top of email
+
         Notifier fullNotifier = new SlackNotifier(
                 new SMSNotifier(baseNotifier));
 
